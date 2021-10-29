d053bc576016607c48240b2d2b64525d7a3602a0
soni-ujustbe <88655561+soni-ujustbe@users.noreply.github.com>
2021-10-29 11:37:43 +0530

    firebase + next js sort

---
+++ component/CreatePost.js
@@ -0,0 +1,65 @@
+// components/CreatePost.js
+import React, { useEffect, useState } from 'react';
+import { collection, addDoc, setDoc, doc, getDocs } from "firebase/firestore";
+//import app from '../config/fire-base';
+import firebaseApp from '../firebaseConfig'
+import { getFirestore } from "firebase/firestore";
+const db = getFirestore();
+
+
+const CreatePost = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  
+  
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let data = {
+      name: 'Los Angeles',
+      state: 'CA',
+      country: 'USA'
+    };
+
+    // Add a new document in collection "cities" with ID 'LA'
+    addDoc(collection(db, "users"), {
+      first: "Ada2",
+      last: "Lovelace2",
+      born: 181555
+    });
+
+
+    console.log({
+      "title": title,
+      "content": content
+    });
+    setTitle('');
+    setContent('');
+  }
+
+  useEffect(() => {
+    const docRef = doc(db, "users");
+    const querySnapshot = getDocs(collection(db, "users"));
+    console.log(querySnapshot);
+  }, [])
+
+  return (
+    <div>
+      <h2>Add Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Title<br />
+          <input type="text" value={title}
+            onChange={({ target }) => setTitle(target.value)} />
+        </div>
+        <div>
+          Content<br />
+          <textarea value={content}
+            onChange={({ target }) => setContent(target.value)} />
+        </div>
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  )
+}
+export default CreatePost;
